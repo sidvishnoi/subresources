124ec0d7f09a310f4f3111caec68b55516a4ecf0
Denis Ah-Kang <denis@w3.org>
2026-02-25 15:41:17 +0400

    restore dom method and use separate script for the network method

--- src/network.ts
+++ src/network.ts
@@ -3,28 +3,7 @@ import puppeteer, {
 	type HTTPResponse,
 } from "puppeteer";
 
-export type ResourceType =
-	| "stylesheet"
-	| "script"
-	| "image"
-	| "favicon"
-	| "video"
-	| "audio"
-	| "object"
-	| "iframe"
-	| "link"
-	| "font"
-	| "manifest";
-
-export interface Resource {
-	type: ResourceType;
-	url: string;
-}
-
-export interface Options {
-	links: boolean;
-	puppeteerOptions?: LaunchOptions;
-}
+import type { Options, Resource, ResourceType } from "./types.js";
 
 function mapResourceType(
 	response: HTTPResponse,
